f24f1bc96a627537cdd087818e8af90a39bf077d
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-05-26 20:53:34 +0400

    Initial release

---
+++ ex01.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../ex01/ft_strncmp.c"
+
+int ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int main(void) {
+	
+	char str_cmp1[] = "ABCDEFAQrSt";
+	char str_cmp2[] = "ABCDEFZQrSt";
+	char empty[] = "";
+	unsigned int ret;
+	
+	printf("Calling ft_strncpy(s1,s2,n);\n");
+    ret=ft_strncmp(str_cmp1,str_cmp2,6);
+	if (ret == 0)
+		printf("Comparing [s1] %s with [s2] %s n = 6 | PASS ret %d\n",str_cmp1,str_cmp2,ret);
+	else
+		printf("Comparing [s1] %s with [s2] %s n = 6 | FAIL ret %d\n",str_cmp1,str_cmp2,ret);
+
+	ret=ft_strncmp(str_cmp2,str_cmp1,7);
+    if (ret == 25)
+		printf("Comparing [s1] %s with [s2] %s n = 7 | PASS ret %d\n",str_cmp2,str_cmp1,ret);
+	else
+		printf("Comparing [s1] %s with [s2] %s n = 7 | FAIL ret %d\n",str_cmp2,str_cmp1,ret);
+
+
+    ret=ft_strncmp(str_cmp1,str_cmp2,8);
+    if ((int)ret == -25)
+		printf("Comparing [s1] %s with [s2] %s n = 8 | PASS ret %d\n",str_cmp1,str_cmp2,ret);
+	else
+		printf("Comparing [s1] %s with [s2] %s n = 8 | FAIL ret %d\n",str_cmp1,str_cmp2,ret);
+
+    ret=ft_strncmp(str_cmp2,empty,7);
+    if (ret == 65)
+		printf("Comparing [s1] %s with [s2] \"\" n = 9          | PASS ret %d\n",str_cmp2,ret);
+	else
+		printf("Comparing [s1] %s with [s2] \"\" n = 9          | FAIL ret %d\n",str_cmp2,ret);
+
+	return (0);
+}
+
---
+++ ex04.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../ex04/ft_strstr.c"
+
+char *ft_strstr(char *str, char *to_find);
+//       strstr(char *haystack, char *needle)
+
+int main(void) {
+
+	char needle[] = "AbuDhabi";
+	char needle1[] = "vmj92293bv";
+	char empty[] = {0x00};
+	char havstack[] = "What Makes AbuDhabi 42 Great";
+
+	printf("str = %s | to_find = %s \n",havstack,needle);
+	printf("Calling: ret = ft_strstr(str,to_find);\n");
+    printf("ret = %s | str = %s | to_find = %s\n\n", ft_strstr(havstack,needle),havstack,needle);
+
+	printf("str = %s | to_find = %s \n",havstack,needle1);
+	printf("Calling: ret = ft_strstr(str,to_find);\n");
+	printf("ret = %s | str = %s | to_find = %s\n\n", ft_strstr(havstack,needle1),havstack,needle1);
+
+	printf("str = %s | to_find = (empty)%s \n",havstack,empty);
+	printf("Calling: ret = ft_strstr(str,to_find);\n");
+	printf("ret = %s | str = %s | to_find = (empty)%s\n\n", ft_strstr(havstack,empty),havstack,empty);
+
+	return 0;
+}
+
